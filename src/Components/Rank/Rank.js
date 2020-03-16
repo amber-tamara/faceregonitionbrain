626a385a84441909d96226258b615e5bae21ac07
amber-tamara <ambertjansen@gmail.com>
2020-03-16 19:17:25 +0000

    Finished front end of website

---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div>
+      <div>
+        <div className="white f3">{"Amber, your current rank is..."}</div>
+      </div>
+      <div>
+        <div className="white f3">{"#5"}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Rank;
